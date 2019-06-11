5284689fe3fd009131a5db5ce40d7b8345fc7229
Jesús Santiago Carrasco Campa <jsanticc@hotmail.com>
2019-06-10 23:07:56 -0700

    . Agregar proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FileUpload from './components/fileUpload';
+
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      user: null
+    }
+
+    this.handleAuth = this.handleAuth.bind(this)
+    this.handleLogOut = this.handleLogOut.bind(this)
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({user});
+    });
+  }
+  
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`ERROR: ${error}`));
+  }
+
+  handleLogOut() {
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user} ha salido`))
+    .catch(error => console.log(`ERROR: ${error}`));
+  }
+
+  renderLoginBtn() {
+    //User logueado
+    if (this.state.user){
+      return(
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <p>Hola {this.state.user.displayName}!</p>
+          <FileUpload/>
+          <button onClick={this.handleLogOut}>Log Out</button>
+        </div>
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>Login w/Google</button>
+      );
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          {this.renderLoginBtn()}
+        </header>
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+//ADD YOUR FIREBASE CREDENTIALS
+
+firebase.initializeApp({
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
